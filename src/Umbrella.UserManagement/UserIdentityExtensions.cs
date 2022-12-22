5dfdd48c8fff5b9c482b9701c8d24eb2ac0b5e10
Francesco Garavaglia <Francesco.Garavaglia@outlook.it>
2022-12-23 00:30:07 +0100

    removing extensions method related to security

--- src/Umbrella.UserManagement/UserIdentityExtensions.cs
+++ src/Umbrella.UserManagement/UserIdentityExtensions.cs
@@ -28,46 +28,5 @@ namespace Umbrella.UserManagement
             return "https://www.tutorialrepublic.com/examples/images/avatar/1.jpg";
         }
 
-        /// <summary>
-        /// Checks if user is ADmin or not, based on his roles
-        /// </summary>
-        /// <param name="principal"></param>
-        /// <returns></returns>
-        public static bool IsAdministrator(this ClaimsPrincipal principal)
-        {
-            if (principal.Identity == null)
-                return false;
-
-            if (!principal.Identity.IsAuthenticated)
-                return false;
-
-            var roleClaims = principal.Claims.Where(x => x.Type == ClaimTypes.Role).ToList();
-            if (roleClaims.Any(x => x.Value == "ADMIN"))
-                return true;
-            return false;
-        }
-
-        /// <summary>
-        /// Checks if user is ADmin or not, based on his roles
-        /// </summary>
-        /// <param name="principal"></param>
-        /// <returns></returns>
-        public static bool HasRoleOf(this ClaimsPrincipal principal, string role)
-        {
-            if (String.IsNullOrEmpty(role))
-                return false;
-
-            if (principal.Identity == null)
-                return false;
-
-            if (!principal.Identity.IsAuthenticated)
-                return false;
-
-            var roleClaims = principal.Claims.Where(x => x.Type == ClaimTypes.Role).ToList();
-            if (roleClaims.Any(x => x.Value == role))
-                return true;
-
-            return false;
-        }
     }
 }
